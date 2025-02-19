0e06e352ae696c5911caa1facd0f4310952208a4
Ildegar <illianclase12345678@gmail.com>
2025-02-19 20:51:11 +0000

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,7 @@ use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('https://local-events-front-green.vercel.app/');
+    return redirect('https://local-events-front-green.vercel.app/');
 });
 
 Route::get('/dashboard', function () {
