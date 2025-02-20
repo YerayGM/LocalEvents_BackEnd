b78152091347b7ce15c26c0e930ee21839771707
Ildegar <illianclase12345678@gmail.com>
2025-02-20 18:26:16 +0000

    Actualizacion ruta del front

--- config/cors.php
+++ config/cors.php
@@ -3,7 +3,7 @@
 return [
     'paths' => ['api/*', 'sanctum/csrf-cookie'],
     'allowed_methods' => ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
-    'allowed_origins' => ['https://local-events-front-green.vercel.app/'],
+    'allowed_origins' => ['https://rafael.informaticamajada.es'],
     'allowed_headers' => ['Content-Type', 'Authorization'],
     'max_age' => 10,
     'supports_credentials' => true,
