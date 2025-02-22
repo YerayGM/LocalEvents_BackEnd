ccee6e1b4b6b63bd0913e9150ec99523aa0687d7
YerayGM <yeraygarciamartinez5@gmail.com>
2025-02-22 23:41:24 +0000

    Revert "Merge pull request #42 from Sr-Stroncio/development"
    This reverts commit 18ff22a48ba441036fa1b4d84fb4560b2a779ba1, reversing
    changes made to 876c98dd2e9648c55337ade87c7a619154c1787c.

--- resources/views/profile/partials/update-profile-information-form.blade.php
+++ resources/views/profile/partials/update-profile-information-form.blade.php
@@ -1,5 +1,9 @@
 <section>
     <header>
+        <h2 class="text-lg font-medium text-gray-900">
+            {{ __('Información del Perfil') }}
+        </h2>
+
         <p class="mt-1 text-sm text-gray-600">
             {{ __("Actualiza la información de tu perfil y dirección de correo electrónico.") }}
         </p>
