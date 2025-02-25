2f9eb147df0eba6e1bab4a16422ed328da1898e8
Sr_Stroncio <razukmamo@gmail.com>
2025-02-25 18:38:40 +0000

    arreglando el error de dropdonw y poniendo nueva ruta

--- resources/js/app.jsx
+++ resources/js/app.jsx
@@ -2,6 +2,10 @@ import ReactDOM from 'react-dom/client';
 import Main from './components/Main';
 import '../css/app.css';
 
+import Alpine from 'alpinejs';
+window.Alpine = Alpine;
+Alpine.start();
+
 ReactDOM.createRoot(document.getElementById('app')).render(
     <Main />
 );
