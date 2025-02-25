83bfae7346381059847253096c9f03736091f7f3
Ildegar <illianclase12345678@gmail.com>
2025-02-25 21:40:22 +0000

    Middleware implementation

--- config/cors.php
+++ config/cors.php
@@ -3,7 +3,7 @@
 return [
     'paths' => ['api/*', 'sanctum/csrf-cookie'],
     'allowed_methods' => ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
-    'allowed_origins' => ['https://rafael.informaticamajada.es', 'http://localhost:5173'],
+    'allowed_origins' => ['https://rafael.informaticamajada.es'],
     'allowed_headers' => ['Content-Type', 'Authorization', 'X-XSRF-TOKEN'],
     'max_age' => 10,
     'supports_credentials' => true,
