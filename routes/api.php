96b0b2347eea841d16e3b7c0858bdb68f0b6cef1
Ildegar <illianclase12345678@gmail.com>
2025-02-26 20:04:39 +0000

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -34,7 +34,7 @@ Route::group(['as' => 'api.'], function () {
     Orion::belongsToResource('comments', 'post', CommentController::class);
     Orion::belongsToResource('events', 'inscribes', EventController::class);
     Orion::belongsToResource('events', 'asistes', EventController::class);
-})->middleware(['auth', 'verified']);
+});
 
 Route::post('/login', [AuthenticatedSessionController::class, 'store']);
 
